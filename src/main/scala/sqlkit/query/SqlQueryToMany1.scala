420c8a7b658d126d8850da090f1d21cd2f2691a4
Benoit Ponsero <benoit.ponsero@pixlstudio.fr>
2025-09-30 14:34:39 +0200

    better perf for oneToMany

--- src/main/scala/sqlkit/query/SqlQueryToMany1.scala
+++ src/main/scala/sqlkit/query/SqlQueryToMany1.scala
@@ -4,6 +4,7 @@ import sqlkit.SqlRow
 import sqlkit.param.SqlParam
 import sqlkit.session.SqlSession
 
+import scala.collection.immutable.VectorMap
 import scala.util.Try
 
 case class SqlQueryToMany1[R, R1](
@@ -21,21 +22,18 @@ case class SqlQueryToMany1[R, R1](
     this.copy(params = this.params ++ params.toList)
   }
 
-  def list(implicit session: SqlSession) = {
+  def list(implicit session: SqlSession): List[(R, List[R1])] = {
     exec { rs =>
-      rs.foldLeft(Map.empty[R, List[R1]]) { case (all, row) =>
+      rs.foldLeft(VectorMap.empty[R, List[R1]]) { case (all, row) =>
         val o = one(row)
         val item = all.getOrElse(o, List.empty[R1])
-        all + (
-          o -> (
-            item ++ Try { toMany1(row) }.toOption
-          )
-        )
+
+        all.updated(o, item ++ Try { toMany1(row) }.toOption)
       }.toList
     }
   }
 
-  def listSets(implicit session: SqlSession) = {
+  def listSets(implicit session: SqlSession): List[(R, Set[R1])] = {
     list.map { case (k, toMany1) =>
       (k, Set.from(toMany1))
     }
